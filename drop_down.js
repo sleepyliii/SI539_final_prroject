00f362359bdf7853991926433f3debe44af58539
JiaouCH <143961489+JiaouCH@users.noreply.github.com>
2024-04-23 18:36:11 -0400

    Update drop_down.js

--- drop_down.js
+++ drop_down.js
@@ -1,17 +1,15 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  var dropdown = document.getElementById("myDropdown");
+  dropdown.classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdownList = document.getElementsByClassName("dropdown-content");
+    Array.from(dropdownList).forEach(function(dropdown) {
+      if (dropdown.classList.contains('show')) {
+        dropdown.classList.remove('show');
       }
-    }
+    });
   }
+};
